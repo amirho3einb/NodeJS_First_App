4b1116806b4384b29778f433692871cee531a44e
amirhossinbalagar <amirhossinbalagar@gmail.com>
2023-02-08 19:19:54 +0330

    create server

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+// function rqListener(req, res) {}
+// http.createServer(rqListener);
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="post"><input type="text" name="messsage"><button type="submit">send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
