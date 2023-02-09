9c5b633be0152035f3af977d0536403eaf03f62a
amirhossinbalagar <amirhossinbalagar@gmail.com>
2023-02-09 14:40:05 +0330

    diffrent ways to export module

--- routes.js
+++ routes.js
@@ -36,4 +36,15 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-module.exports = requestHandler;
+// module.exports = requestHandler;
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: "Some hard coded text",
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = "Some Text";
+
+exports.handler = requestHandler;
+exports.someText = "Some hard coded text";
