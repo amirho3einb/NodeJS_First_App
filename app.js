9b3ebb18d7706011cf2d14e96e8d6eb20deedd2a
amirhossinbalagar <amirhossinbalagar@gmail.com>
2023-02-09 12:15:42 +0330

    033 Parsing Request Bodies

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require("http");
+const fs = require("fs");
 
 // function rqListener(req, res) {}
 // http.createServer(rqListener);
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -12,6 +15,21 @@ const server = http.createServer((req, res) => {
       '<body><form action="/message" method="post"><input type="text" name="messsage"><button type="submit">send</button></form></body>'
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
