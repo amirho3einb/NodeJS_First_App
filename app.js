63ea944da3e2530fafea8d2e101f7340e2182a5a
amirhossinbalagar <amirhossinbalagar@gmail.com>
2023-02-11 15:32:26 +0330

    Adding Middleware

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("I am in the middelware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("I am in another the middelware");
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
