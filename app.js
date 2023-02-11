407e437e88cfad9208b4c7adc563adc85190db3b
amirhossinbalagar <amirhossinbalagar@gmail.com>
2023-02-11 15:44:23 +0330

    Handling Different Routes

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("I am in the middelware");
-  next();
+app.use("/add-product", (req, res, next) => {
+  console.log("I am in another the middelware product");
+  res.send("<h1>The Add product Page</h1>");
 });
 
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
   console.log("I am in another the middelware");
+  res.send("<h1>Hello from Express!</h1>");
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
